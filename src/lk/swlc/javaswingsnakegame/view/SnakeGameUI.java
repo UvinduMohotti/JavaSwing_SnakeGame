64e7cbc20be74b91b3f129a38ccffbbd160d9948
Uvindu Mohotti <uvindu@nemotechno.com>
2021-01-04 01:23:31 +0530

    first commit

---
+++ src/lk/swlc/javaswingsnakegame/view/SnakeGameUI.java
@@ -0,0 +1,16 @@
+/*
+ *Time   :- 1:21 AM
+ *Author :- Uvindu Mohotti
+ *Special Thing :-
+ */
+
+package lk.swlc.javaswingsnakegame.view;
+
+import javax.swing.*;
+
+public class SnakeGameUI extends JFrame {
+
+    public static void main(String[] args) {
+
+    }
+}
